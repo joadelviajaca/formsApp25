97ebb04cfb45f2977e312c55b20cd477ac54ddec
joadelvia <joadelvia@gmail.com>
2025-02-07 12:36:25 +0100

    Añadir favoritos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Persona } from '../../interfaces/persona';
+import { Favorito, Persona } from '../../interfaces/persona';
 import { JsonPipe } from '@angular/common';
 
 @Component({
@@ -12,14 +12,31 @@ export class DinamicosComponent {
 
   @ViewChild('myForm') myForm!: NgForm;
 
+  name: string = "";
   persona : Persona = {
     nombre: "Joaquín", 
     favoritos: [{id: 1, nombre: "Fornite"}, {id: 2, nombre: "Super Mario"}]
   }
 
+  id: number = 3;
+
   notValid(field: string): boolean {
     console.log(field)
     return this.myForm?.controls[field].invalid && this.myForm?.controls[field].touched
+  }
+
+  agregarFavorito(){
+    const nuevoFavorito: Favorito = {
+      id: this.id,
+      nombre: this.name
+    }
+    this.persona.favoritos.push(nuevoFavorito);
+    this.id++;
+    this.name = '';
+  }
+
+  borrarFavorito(id: number){
+    this.persona.favoritos = this.persona.favoritos.filter(favorito => favorito.id !== id)
   }
 
   submit(){
