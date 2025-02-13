7c12f70f804355f67522c3928c762a6b4e7a73b0
joadelvia <joadelvia@gmail.com>
2025-02-13 08:28:24 +0100

    template terminados

--- src/app/interfaces/persona.ts
+++ src/app/interfaces/persona.ts
@@ -7,3 +7,8 @@ export interface Favorito{
     id: number;
     nombre: string;
 }
+
+export interface Person {
+    genre: string;
+    notifications: boolean;
+}
--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Person } from '../../interfaces/persona';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './switches.component.html'
 })
 export class SwitchesComponent {
+
+  person: Person = {
+    genre: "F",
+    notifications: true
+  }
+
+  terms: boolean = false;
+
+  submit(){
+    console.log('Se ha enviado el formulario');
+  }
 
 }
