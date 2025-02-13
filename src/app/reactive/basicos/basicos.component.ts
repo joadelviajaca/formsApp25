4bfad184c2c2de63de04b77270a2859481c5fa63
joadelvia <joadelvia@gmail.com>
2025-02-13 10:01:24 +0100

    Reactive dinamicos finalizado

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -17,10 +17,28 @@ export class BasicosComponent {
   // constructor(private fb: FormBuilder){}
 
   private fb: FormBuilder = inject(FormBuilder);
-  
+
   myForm: FormGroup = this.fb.group({
-    nombre: ['Patatas', [Validators.required, Validators.minLength(3)]]
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    precio: [0, [Validators.required, Validators.min(0)]],
+    existencias: [0, [Validators.required, Validators.min(0)]]
   })
 
+  isValidField(field: string){
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  submit(){
+    if(this.myForm.valid){
+      console.log('Formulario enviado');
+      this.myForm.reset({
+        nombre: 'Patatas',
+        precio: 0
+      })
+    }
+    else{
+      this.myForm.markAllAsTouched();
+    }
+  }
 
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './dinamicos.component.html'
 })
 export class DinamicosComponent {
+
+  private fb: FormBuilder = inject(FormBuilder);
+
+  newFavourite: FormControl = this.fb.control('', Validators.required);
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    favourites: this.fb.array([
+      ['Monkey Island', Validators.required],
+      ['Super Mario', Validators.required]
+    ], Validators.required)
+  })
+  
+  get favourites(){
+    return this.myForm.get('favourites') as FormArray
+  }
+
+  isValidField(field: string){
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
+
+  addFavourite(){
+    if (this.newFavourite.valid){
+      // this.favourites.push(this.newFavourite);
+      let newControl = this.fb.control(this.newFavourite.value, Validators.required);
+      this.favourites.push(newControl)
+      this.newFavourite.reset()
+    }
+  }
+
+  delete(i: number){
+    this.favourites.removeAt(i);
+  }
+  
+
+  submit(){
+    
+    if(this.myForm.valid){
+      console.log('Formulario enviado');
+      this.myForm.reset({
+        name: ''
+      })
+    }
+    else{
+      this.myForm.markAllAsTouched();
+    }
+  }
 
 }
